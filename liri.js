018ffe07ea8b6fad2fa1e4af7521ee5d5dfdc85f
Mike Smith <smith.mike@mac.com>
2019-08-07 21:15:16 -0400

    Using fs to create and append to a log file.

--- liri.js
+++ liri.js
@@ -1,13 +1,13 @@
+// requiring all files and packages
 require("dotenv").config();
 var fs = require("fs");
 var axios = require("axios");
 var moment = require('moment');
 var Spotify = require("node-spotify-api");
-
 var keys = require("./keys.js");
 
-// var spotify = new Spotify(keys.spotify);
 
+// taking in the user input so that I can pass it in to the functions below
 var modifiedArray = [];
 for (let i = 2; i < process.argv.length; i++) {
     modifiedArray.push(process.argv[i]);
@@ -18,7 +18,22 @@ for (let i = 1; i < modifiedArray.length; i++) {
     parameterArray.push(modifiedArray[i]);
 }
 var parameter = parameterArray.join("+");
-console.log(parameter);
+// console.log(parameter);
+
+// Creates log file to be written too with header
+fs.appendFile('log.txt', "node liri.js" + " " + action + " " + parameter + ", ", function (err) {
+    // If the code experiences any errors it will log the error to the console.
+    if (err) {
+        return console.log(err);
+    }
+
+    // Otherwise, it will print: "log.txt was updated!"
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    console.log("");
+    console.log("log.txt was updated!");
+    console.log("");
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+});
 
 
 // We will then create a switch-case statement (if-then would also work).
@@ -39,9 +54,20 @@ switch (action) {
     case "do-what-it-says":
         textFile();
         break;
+
+    default:
+        console.log("============================================================")
+        console.log("");
+        console.log("I'm sorry, " + action + " is not a command that I am familiar with. Please use one of the following commands:");
+        console.log("node liri.js concert-this + artist -- (Looks for upcoming concerts for specified artists)");
+        console.log("node liri.js do-what-it-says -- (Looks for a ramdom thing to do)");
+        console.log("node liri.js movie-this + movie title -- (Looks for the moive title you entered and returns info from OMDB)");
+        console.log("node liri.js spotify-this-song + song title -- (Searches Spotify for the song title you entered)");
+        console.log("");
+        console.log("============================================================")
 }
 
-// If the "total" function is called...
+// concert-this function
 function bands(parameter) {
 
     // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
@@ -51,9 +77,11 @@ function bands(parameter) {
         .then(function (response) {
             for (i = 0; i < 10; i++) {
                 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                console.log("");
                 console.log("Venue: " + response.data[i].venue.name);
                 console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
                 console.log("Concert Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                console.log("");
                 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             }
 
@@ -79,7 +107,9 @@ function bands(parameter) {
 
 }
 
-// If the "Deposit" function is called...
+
+
+// spotify-this-song function
 function songs(keys, Spotify, parameter) {
     if (!parameter) {
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -101,7 +131,9 @@ function songs(keys, Spotify, parameter) {
             console.log("Album: " + data.tracks.items[0].album.name);
             console.log("");
             console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            console.log("");
             console.log("Pretty cool how bout you search for another one");
+            console.log("");
             console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         })
         .catch(function (err) {
@@ -110,13 +142,17 @@ function songs(keys, Spotify, parameter) {
 
 }
 
-// If the "Withdraw" function is called
+
+
+// movie-this function
 function movies(parameter) {
     if (!parameter) {
         console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log("");
         console.log("If you haven't watched 'Mr. Nobody,' then you should: ");
         console.log("Heres a link to IMDB: http://www.imdb.com/title/tt0485947/");
-        console.log("It's on Netflix!");
+        console.log("It's also on Netflix!");
+        console.log("");
         parameter = "Mr. Nobody";
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy";
@@ -129,21 +165,16 @@ function movies(parameter) {
         .then(
             function (response) {
                 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                console.log("");
                 console.log("Title: " + response.data.Title);
                 console.log("Release Year: " + response.data.Year);
                 console.log("IMDB Rating: " + response.data.imdbRating);
-                for (i = 0; i < response.data.Ratings; i++) {
-                    console.log(response.data.Ratings[i])
-                    // if(response.data.Ratings[i].source === "Rotten Tomatoes"){
-                    //     console.log("Rotten Tomatoes Rating: " + response.data.Ratings[i].source.value);
-                    // }else{
-                    //     console.log("Rotten Tomatoes Rating: N/A");
-                    // }
-                }
+                console.log("Rotten Tomatoes Rating : " + response.data.Ratings[1].Value);
                 console.log("Crountry it was produced in: " + response.data.Country);
                 console.log("Language it was released in: " + response.data.Language);
                 console.log("Plot: " + response.data.Plot);
                 console.log("Actors: " + response.data.Actors);
+                console.log("");
                 console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             })
         .catch(function (error) {
@@ -170,19 +201,19 @@ function movies(parameter) {
 }
 
 
-// If the "Lotto" function is called
+// do-what-it-says
 function textFile() {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
         var randomArray = data.split(",");
-        console.log(randomArray);
+        // console.log(randomArray);
         var parameterArray = []
         if (randomArray[0] === "spotify-this-song") {
             for (let i = 1; i < randomArray.length; i++) {
                 parameterArray.push(randomArray[i]);
             }
             var parameter = parameterArray.join("+");
-            console.log(parameter);
+            // console.log(parameter);
             songs(keys, Spotify, parameter);
         }
 
@@ -191,10 +222,5 @@ function textFile() {
 
 }
 
-// * `concert-this`
 
-// * `spotify-this-song`
 
-// * `movie-this`
-
-// * `do-what-it-says`
